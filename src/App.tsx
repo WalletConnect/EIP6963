eae9c00f4e925674b9c71ef90369212c4053e135
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2023-05-25 15:23:19 +0530

    fix: icon position

--- src/App.tsx
+++ src/App.tsx
@@ -254,25 +254,27 @@ function App() {
                 initial="initial"
                 animate="animate"
                 exit="exit"
-                className="flex items-center gap-2 text-zinc-700"
+                className="flex items-center gap-2 leading-snug text-zinc-700"
               >
+                {window.ethereum && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    className="flex-shrink-0 w-4 h-4"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+                    />
+                  </svg>
+                )}
                 {window.ethereum
                   ? `No EIP-6963 compatible providers found`
-                  : `No window.ethereum providers found`}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={2}
-                  stroke="currentColor"
-                  className="w-4 h-4"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-                  />
-                </svg>
+                  : `No window.ethereum providers found.\nMake sure you have a wallet installed.`}
               </motion.p>
             )}
           </AnimatePresence>
